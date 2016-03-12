02ac51d5b3dd0dace63ec252b99b9f517645e660
ecrmnn <danieleckermann@gmail.com>
2016-03-12 02:16:30 +0100

    using passlawl.js. cli specific tests. added chalk for error msgs

--- test/index.js
+++ test/index.js
@@ -2,19 +2,60 @@
 
 const chai = require('chai');
 const expect = require('chai').expect;
-const en = require('../src/locale/en');
-const no = require('../src/locale/en');
+const exec = require("child_process").exec;
+let captured_stdout = '';
 
-describe('Generator test', function () {
-  'use strict';
-
-  it('returns array', function () {
-    expect(en).to.be.instanceof(Array);
-    expect(no).to.be.instanceof(Array);
+describe('Running CLI without any params', function () {
+  before(function (done) {
+    exec('node ./src', function (error, stdout, stderr) {
+      captured_stdout = stdout;
+      done();
+    });
   });
 
-  it('returned array contains 4 words', function () {
-    expect(en).to.have.length(4);
-    expect(no).to.have.length(4);
+  it('returns a password', function () {
+    expect(captured_stdout).to.be.a('string');
+  });
+});
+
+describe('Running CLI help command', function () {
+  before(function (done) {
+    exec('node ./src --help', function (error, stdout, stderr) {
+      captured_stdout = stdout;
+      done();
+    });
+  });
+
+  it('returns available options', function () {
+    expect(captured_stdout).to.have.string('Options:');
+    expect(captured_stdout).to.have.string('--help');
+    expect(captured_stdout).to.have.string('--version');
+    expect(captured_stdout).to.have.string('--locale');
+  });
+});
+
+describe('Running CLI with locale', function () {
+  before(function (done) {
+    exec('node ./src --locale no', function (error, stdout, stderr) {
+      captured_stdout = stdout;
+      done();
+    });
+  });
+
+  it('returns a password in chosen language', function () {
+    expect(captured_stdout).to.be.a('string');
+  });
+});
+
+describe('Running CLI with invalid locale', function () {
+  before(function (done) {
+    exec('node ./src --locale NotValidLocale', function (error, stdout, stderr) {
+      captured_stdout = stdout;
+      done();
+    });
+  });
+
+  it('returns an error message', function () {
+    expect(captured_stdout).to.have.string('Given locale not supported. Supported locales:');
   });
 });
